0102eb975bd9df32141ce36ecdc82cde02bb16f7
Aniruthan123 <66328030+Aniruthan123@users.noreply.github.com>
2020-11-19 21:08:01 +0530

    Update Banking.py

--- Banking.py
+++ Banking.py
@@ -1,6 +1,8 @@
-import random
+mport random
 
 import time 
+
+
 
 print("Welcome To Mallaya Bank:")
 time.sleep(0.44)
@@ -36,19 +38,22 @@ print("You will have a minimum account balance of Rs.500")
 anum+=500
 time.sleep(2)
 
-print("Welcome Again:")
-time.sleep(2)
-print("ENTER 1 TO ATTAIN A DEBIT CARD OR A CREDIT CARD")
-print("ENTER 2 TO APPLY FOR A LOAN:")
-    
-print("ENTER 3 FOR  WITHDRAWLS OR DEPOSITS")
-print("ENTER 7 FOR FOREIGN EXCHANGE")
-print("ENTER 8 TO KNOW ABOUT YOUR A/C STATUS/BALANCE  ")
-    
-output=int(input("Enter the required number:"))
+def welcomeMessage():
+
+    print("Welcome Again:")
+    time.sleep(2)
+    print("ENTER 1 TO ATTAIN A DEBIT CARD OR A CREDIT CARD")
+    print("ENTER 2 TO APPLY FOR A LOAN:")
+        
+    print("ENTER 3 FOR  WITHDRAWLS OR DEPOSITS")
+    print("ENTER 7 FOR FOREIGN EXCHANGE")
+    print("ENTER 8 TO KNOW ABOUT YOUR A/C STATUS/BALANCE  ")
+        
+    output=int(input("Enter the required number:"))
+    return output
    
 
-
+output = welcomeMessage()
 if output==1:
     DE=""
     re=""
@@ -77,6 +82,7 @@ if output==1:
                     numeroid = (random.randint(0,9))
                     oo0 += str(numeroid) 
                 print(oo0,"is your pin number")
+                welcomeMessage()
         else:
             eds=int(input("CREDIT CARD CHECK:Enter 1 if you are 21,and with  a job with a 6 digit income,,,if you have an exception press 2"))
             if eds==1:
@@ -116,13 +122,50 @@ elif output==2:
         if lml<=400000:
             print("The loan amount has been transferred to your account")
             anum+=lml
-            
-      
-
-
-
+            gello=int(input("Enter 1 to continue else enter to 2 to stop"))
+            if gello==2:
+                print("end")
+            else:
+                print("Enter a different  number")
+                welcomeMessage()
+elif output==3:
+    print("Your account balance is ",anum)
+    the=int(input("Enter 1 to withdrawl or 2 to deposit"))
+    if the==1:
+        def ert():
+         trey=int(input("ENTER THE AMOUNT YOU WISH TO WITHDRAWL:"))
          
-    
+         if trey>anum:
+            print("Insufficient Funds")
+            ert()
+         else:
+            anum=anum+trey   
+            print("Your withdrawl is succesful and your account balance is:",anum) 
+            welcomeMessage()         
 
+    else:
+        Trey=int(input("ENTER THE AMOUNT YOU WISH TO DEPOSIT:"))
+        anum=anum+Trey
+        print("Your new account balance is:",anum)
+        welcomeMessage()
 
+elif output==7:
+    print("We have three foreign currencies to exchange;US dollars(23),Euros(34),Australian dollars(44)")
+    tyu=int(input("Enter the currenvey code given from above:"))
+    if tyu==23:
+        opi=int(input("Enter the INR amount you are willing to offer for the exchange:"))
+        fv=opi/76
+        print("The amount of dollars you will be getiing is:",fv)
+    elif tyu==34:
+        opt=int(input("Enter the INR amount you are willing to offer for the exchange:"))
+        fvy=opi/88
+        print("The amount of euros you will be getiing is:",fvy)
+    elif tyu==44:
+        opu=int(input("Enter the INR amount you are willing to offer for the exchange:"))    
+        fgh=opu/55
+        print("The amount of aus dollars you will be getiing is:",fgh)
+    welcomeMessage()  
 
+elif output==8:
+    print("Your account balance is",anum)    
+    welcomeMessage()
